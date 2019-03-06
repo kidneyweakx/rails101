11ad0938e79df05be4d8aab63d837e176f8b3354
kidneyweakx <kidneyweakx@gmail.com>
2019-03-06 12:25:56 +0800

    implement group#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,6 +20,14 @@ class GroupsController < ApplicationController
     @group.save
   end
 
+  def update
+    @group = Group.find(params[:id])
+
+    @group.update(group_params)
+
+    redirect_to groups_path, notice: "Update Sucess"
+  end
+
   private
 
   def group_params
